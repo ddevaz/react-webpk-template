20d420f9d3f272a5fe8a56859668421991d26995
Dmitri De Vaz <dimology@gmail.com>
2015-09-05 14:05:10 -0700

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+    ],
+  },
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+}
+
